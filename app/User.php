7653decb1b0bc1b3954c317e8729720612756685
jilcimar.fernandes <jilcimar.fernandes0267@gmail.com>
2019-10-02 20:26:44 -0300

    models para quem utiliza a biblioteca Entrust

--- app/User.php
+++ app/User.php
@@ -78,4 +78,13 @@ class User extends Authenticatable
     {
         return $this->papel->general_slug == $roleGeneralSlug;
     }
+
+    /**
+     * Relacionamento para quem utiliza a biblioteca Entrust
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class,'role_user', 'user_id', 'role_id');
+    }
 }
